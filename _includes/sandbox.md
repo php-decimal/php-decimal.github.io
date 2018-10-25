38c7bea5d287a1bd2b75f94ef38aab06832660e9
Rudi Theunissen <rudolf.theunissen@gmail.com>
2018-10-24 19:47:44 -0700

    Added very basic sandbox

--- _includes/sandbox.md
+++ _includes/sandbox.md
@@ -1,1 +1,22 @@
 ## Sandbox
+
+<div id="sandbox-container">
+    <div id="sandbox-editor-container">
+        <div id="sandbox-editor"><!--
+
+--><?php
+use Decimal\Decimal;
+
+$a = new Decimal("0.1");
+$b = new Decimal("7.0");
+
+echo $a / $b;
+</div>
+    </div>
+    <div id="sandbox-buttons">
+        <button id="sandbox-button-run" type="button">Run</button>
+        <button id="sandbox-button-example" type="button">Example</button>
+        <button id="sandbox-button-clear" type="button">Reset</button>
+    </div>
+    <textarea id="sandbox-result" readonly></textarea>
+</div>
--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,9 @@
-var menu      = document.getElementsByTagName('menu')[0];
-var toggle    = document.getElementById('menu');
-var menuItems = document.querySelectorAll('menu a');
+(function() {
+
+var menu      = $('menu').eq(0);
+var toggle    = $('menu');
+var menuItems = $('menu a');
+
 
 /**
  * Hides the mobile menu.
@@ -13,12 +16,10 @@ function hideMobileMenu() {
 }
 
 // We want to make sure the menu is hidden if we're accessing an anchor.
-window.addEventListener('hashchange', hideMobileMenu);
+$(window).on('hashchange', hideMobileMenu);
 
 // When clicking a menu link, make sure the menu disappears.
-for (var i = 0; i < menuItems.length; i++) {
-    menuItems[i].addEventListener('click', hideMobileMenu);
-}
+menuItems.on('click', hideMobileMenu);
 
 // Enable scroll spy with a delay to allow for anchor scrolling.
 var timeout = window.location.hash ? 1000 : 0;
@@ -44,3 +45,116 @@ if (window.location.hash == false) {
 }
 
 hideMobileMenu();
+
+/**
+ * Sandbox
+ */
+var sandbox = ace.edit("sandbox-editor", {
+    highlightActiveLine: false,
+    cursorStyle: 'smooth',
+    showGutter: false,
+    highlightGutterLine: false,
+    scrollPastEnd: false,
+    theme: 'ace/theme/tomorrow',
+    mode: "ace/mode/php",
+    useSoftTabs: true,
+    tabSize: 4,
+    autoScrollEditorIntoView: false,
+    vScrollBarAlwaysVisible: false,
+    fontSize: "1em",
+    fontFamily: "Fira Code",
+    wrap: true,
+    animatedScroll: false,
+    printMargin: false,
+    showInvisibles: false,
+    displayIndentGuides: false,
+    showFoldWidgets: false,
+    selectionStyle: "line",
+});
+
+
+
+var insertTick;
+
+var blankStart = "<?php\nuse Decimal\\Decimal;\n\n";
+
+var examples = [
+    blankStart + '$a = new Decimal("0.1");\n'
+               + '$b = new Decimal("0.2");\n'
+               + '$c = $a + $b - "0.3";\n'
+               + '\n'
+               + 'echo $c;',
+
+    blankStart + '$a = new Decimal("1", 32);\n'
+               + '$b = new Decimal("7");\n'
+               + '\n'
+               + 'print_r($a / $b);',
+];
+
+
+function setResult(result) {
+    $("#sandbox-result").text(result).show();
+}
+
+function clearResult() {
+    $("#sandbox-result").text("");
+}
+
+function hideResult() {
+    $("#sandbox-result").hide();
+}
+
+function evaluate() {
+    clearResult();
+    $.post('http://192.241.150.198', {input: sandbox.getValue()}, function(result) {
+        setResult(result);
+    });
+}
+
+function clearEditor() {
+    sandbox.setValue("");
+    sandbox.focus();
+}
+
+function resetSandbox() {
+    clearEditor();
+    clearResult();
+    hideResult();
+    setAnimatedText(blankStart);
+    sandbox.focus();
+}
+
+function getExample() {
+    var example = examples.pop();
+    examples.unshift(example);
+
+    return example;
+}
+
+function setAnimatedText(text, callback) {
+    clearEditor();
+    clearResult();
+    clearTimeout(insertTick);
+
+    var i = 0;
+    insertTick = setInterval(function() {
+        if (i < text.length) {
+            sandbox.insert(text.charAt(i++));
+        } else {
+            clearTimeout(insertTick);
+            if (callback) {
+                callback();
+            }
+        }
+    }, 5);
+}
+
+function setExample() {
+    setAnimatedText(getExample(), evaluate);
+}
+
+$("#sandbox-button-run").click(evaluate);
+$("#sandbox-button-example").click(setExample);
+$("#sandbox-button-clear").click(resetSandbox);
+
+})();
